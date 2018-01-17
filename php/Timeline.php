58d6bc72c9241a6a947c6a2ec742fedf7a6730cb
erbound <erik.bossert@online.de>
2018-01-17 15:43:04 +0100

    update functions.php
    Signed-off-by: erbound <erik.bossert@online.de>

--- php/Timeline.php
+++ php/Timeline.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: z003nrrw
- * Date: 17.01.2018
- * Time: 13:45
- */
+include_once ("functions.php");
+echo get_head();
+?>
+    <body>
+
+<div class="container">
+<?php
+
+echo get_nav();
+?>
+    <header>
+        <div class="jumbotron">
+            <h1>Milestones</h1>
+        </div>
+    </header>
+
+    <div class="row">
+        <div class="col-lg-9">
+            <main>
+                <?php
+
+                ?>
+            </main>
+        </div>
+        <div class="col-lg-3">
+            <aside></aside>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <footer>/footer>
+        </div>
+    </div>
+
+</div>
+    </body>
+</html>
---
+++ php/functions.php
@@ -0,0 +1,10 @@
+<?php
+function get_head()
+{
+    return file_get_contents("php/html/head.html");
+}
+
+function get_nav()
+{
+    return file_get_contents("php/html/nav.html");
+}
