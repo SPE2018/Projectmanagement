d78c25f902bc9901c3c33665634f57321877ff13
Anakorth <nicolas.betzler@education-siemens.com>
2018-01-18 15:15:45 +0100

    admin initial commit

---
+++ LogIn/Login.php
@@ -0,0 +1,17 @@
+<?php
+    include_once 'LoginUtility.php';
+    include_once 'util/sql_util.php';
+    //include_once 'functions.php';
+    
+    //get_Head();
+?>
+<body>
+    <h1>Sign in</h1>
+    <div name="loginform">
+        <?php
+            Login::createLoginForm();
+        ?>
+        <a href="register.php">register</a>
+    </div>
+
+</body>
