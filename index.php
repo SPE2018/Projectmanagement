34434aaeb65ccf703264b5380189767fcf3667cf
svdragster <svdragster@gmail.com>
2018-02-01 15:49:55 +0100

    index

--- index.php
+++ index.php
@@ -3,9 +3,12 @@ include_once("util/sql_util.php");
 include_once("php/functions.php");
 echo get_head();
 
+
+
 echo get_navtop();
 foreach($Projects as $v)
     echo '<a class="dropdown-item" href="projects.php?id='.$v->id.'&name='.$v->name.'&startdate='.$v->createdDate.'&enddate='.$v->endDate.'">'.$v->name.'</a>';
 echo get_navbottom();
 echo get_index_jumbotop();
+echo get_jumbobot();
 ?>
